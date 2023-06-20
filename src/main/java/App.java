86d79da22b77830f164c81f5766b3a4331f40dc9
aiznaz <131270076+aiznaz@users.noreply.github.com>
2023-06-21 01:42:21 +0300

    1st try

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -5,8 +5,21 @@ public class App {
     public static void main(String[] args) {
         ApplicationContext applicationContext =
                 new AnnotationConfigApplicationContext(AppConfig.class);
-        HelloWorld bean =
+        HelloWorld bean1 =
                 (HelloWorld) applicationContext.getBean("helloworld");
-        System.out.println(bean.getMessage());
+        System.out.println(bean1.getMessage());
+        HelloWorld bean2 = (HelloWorld) applicationContext.getBean("helloworld");
+        System.out.println((bean2.getMessage()));
+
+        Cat cat1 = (Cat) applicationContext.getBean("cat");
+        System.out.println(cat1.getCat());
+        Cat cat2 = (Cat) applicationContext.getBean("cat");
+        System.out.println(cat2.getCat());
+
+        System.out.println("Бины helloworld равны?");
+        System.out.println(bean1 == bean2);
+
+        System.out.println("Бины cat равны?");
+        System.out.println(cat1 == cat2);
     }
 }
